cd395cf2de139536eaac8f2c7fcde9aae42ff060
Mr.Jing <877131516@qq.com>
2015-05-21 22:12:05 +0800

    定义路由文件

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,7 +1,39 @@
 <?php
 
-define('ROUTE_BASE', 'att/public');
+define('ROUTE_BASE', 'attendance/public');
 
-$app->get(ROUTE_BASE . '/index', function () use ($app) {
-    return $app->welcome();
-});
+$app->get(ROUTE_BASE . '/index', array(
+    'as' => 'home',
+    'uses' => 'RecordController@getRecord',
+));
+
+$app->get(ROUTE_BASE . '/about', array(
+    'as' => 'about',
+    'uses' => 'OtherController@getAbout',
+));
+
+$app->get(ROUTE_BASE . '/login', array(
+    'as' => 'login',
+    'uses' => 'PassportController@getLogin',
+));
+
+$app->post(ROUTE_BASE . '/login', array(
+    'uses' => 'PassportController@postLogin',
+));
+
+$app->post(ROUTE_BASE . '/logout', array(
+    'uses' => 'PassportController@postLogout',
+));
+
+$app->get(ROUTE_BASE . '/record', array(
+    'as' => 'record',
+    'uses' => 'RecordController@getRecord',
+));
+
+$app->get(ROUTE_BASE . '/record/([0-9]{4})/([0-9]{1,2})', array(
+    'uses' => 'RecordController@getIndex',
+));
+
+$app->post(ROUTE_BASE . '/record', array(
+    'uses' => 'RecordController@postStore',
+));
